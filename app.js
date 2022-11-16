ce5e8ad262a67ff6b59b320a991ce1c433d3aa93
Amit Mankotia <amit100.mankotia@gmail.com>
2022-11-16 12:08:49 +0530

    :fire:ivy

---
+++ app.js
@@ -0,0 +1,20 @@
+const express=require("express")
+const app=express()
+app.get("/",function(req,res){
+res.send("<h1>world 123</h1>")
+})
+app.get("/About",function(req,res){
+res.sendFile("/Views/About.html",{root:__dirname})
+})
+//Redirect
+app.get("/Aboutus",(req,res)=>{
+res.redirect("/About")
+})
+//404
+app.use((req,res)=>{
+    res.status(404)
+    res.sendFile("/Views/404.html",{root:__dirname})
+})
+app.listen(3000,()=>{
+console.log("server is listening");
+})
